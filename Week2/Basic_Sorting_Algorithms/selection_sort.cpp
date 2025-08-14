91b44d5c70517a77f58270004911e07e8b3fc5a2
RahemShakeel <rahemshakeel7@gmail.com>
2025-08-14 18:18:21 +0500

    Create selection_sort.cpp

---
+++ Week2/Basic_Sorting_Algorithms/selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void print(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+    cout << "Sorted Array: ";
+    print(arr, n);
+
+    return 0;
+}
