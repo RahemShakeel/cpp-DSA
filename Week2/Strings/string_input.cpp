f8946d81b2094b2eec0f70ce9f6d228f168b1309
RahemShakeel <rahemshakeel7@gmail.com>
2025-08-19 19:45:16 +0500

    Create string_input.cpp

---
+++ Week2/Strings/string_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+//STRINGS IN C++ (Inputs)
+int main () {
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    cout << str[0] << endl;
+    cout << str[1] << endl;
+    cout << str[2] << endl;
+    return 0;
+}
