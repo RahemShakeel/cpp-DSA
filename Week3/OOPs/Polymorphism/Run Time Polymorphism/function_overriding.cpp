2ac5ab9a4a15cb87844e8f1615d872d21c98ca42
RahemShakeel <rahemshakeel7@gmail.com>
2025-09-01 16:30:05 +0500

    Create function_overriding.cpp

---
+++ Week3/OOPs/Polymorphism/Run Time Polymorphism/function_overriding.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//Function Overriding
+class Parent {
+public:
+    void show() {
+        cout << "parent class show..\n";
+    }
+};
+
+class Child : public Parent {
+public:
+    void show() {
+        cout << "child class show..\n";
+    }
+};
+
+int main () {
+    Child child1;
+
+    child1.show();
+
+    return 0;
+}
