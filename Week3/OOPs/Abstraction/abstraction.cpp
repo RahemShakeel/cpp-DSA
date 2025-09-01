ea6157fd5e98e3557288e56ebc410231f2dd3998
RahemShakeel <rahemshakeel7@gmail.com>
2025-09-01 16:35:32 +0500

    Create abstraction.cpp

---
+++ Week3/OOPs/Abstraction/abstraction.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//Abstract Classes and Pure Virtual Functions
+
+//abstract class
+class Shape {
+public:
+    virtual void draw() = 0; //pure virtual function / abstract function
+};
+
+class Circle : public Shape {
+public:
+    void draw() {
+        cout << "draw circle\n";
+    }
+};
+
+class Square : public Shape {
+public:
+    void draw() {
+        cout << "draw square\n";
+    }
+};
+
+int main () {
+    Circle cir1;
+    cir1.draw();
+
+    Square squ1;
+    squ1.draw();
+
+    return 0;
+}
